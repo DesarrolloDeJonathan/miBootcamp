8f2d1f219d58ff4c40ff50f4087de70a7654836f
Josathan <jonathan.cardozo.dev@gmail.com>
2021-06-07 23:03:07 -0500

    Controlando eventos en React. onClick

--- parte1-react/src/index.js
+++ parte1-react/src/index.js
@@ -4,29 +4,25 @@ import { useState } from 'react'
 const rootElement = document.getElementById("root")
 
 const App = (props) => {
-  const [contadorValue, updateContador] = useState(13)
-  /* const contador = useState(10)
-  const contadorValue = contador[0]
-  const updateContador = contador [1]
-  */
+  const [contadorValue, updateContador] = useState(1)
 
-  //Cosas como esta pueden petar el pc o la web
-  //por que se ejecuta cada vez que lo hace la función
-  // 📌Importante Por que de una forma declarativa esta
-  // Reflejando los cambios en la UI
-  setInterval(() =>{
-    updateContador(contadorValue + 1)
-  }, 3000)
-
-  setTimeout
-
-  console.log("render")
+console.log("render");
 
   return (
   <div>
     <p>El valor del contador es:</p>
     <h1>{contadorValue}</h1>
-    <h2>Mágia de React</h2>
+    <button onClick={() =>
+    // Esta forma más común cuando no se usa el estado anteriror
+    //{updateContador(contadorValue+1)}
+    //Para asegurar que se usara el estado anterior
+      setContador(prevContador => {
+        return prevContador + 1
+      })
+      }
+    >
+      Incrementar
+    </button>
   </div>
   )
 }
