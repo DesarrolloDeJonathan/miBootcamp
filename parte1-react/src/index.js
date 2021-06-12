65a9852e2882b058cffdfdb5ed8abeefe0bbf824
Josathan <jonathan.cardozo.dev@gmail.com>
2021-06-12 17:49:54 -0500

    Reset al stage y quitar un estado #3

--- parte1-react/src/index.js
+++ parte1-react/src/index.js
@@ -4,65 +4,67 @@ import "./styles.css"
 
 const rootElement = document.getElementById("root");
 
-// Delarando componentes para renderizado condicional
 const WarningNotUsed = () => {
   return <h1>Todavia no se ha usado el contador</h1>
 }
 const ListOfClicks = ({clicks}) => {
-  //console.log({clicks}); //Añadimos llaves para más detalles
-  //debugger; //Detendra la ejecucuón en este punto
   return <p>{clicks.join(', ')}</p>
 }
 
-const INITIAL_COUNTER_STATE = {
-  left: 0,
-  right:0,
-  mensaje: 'Mensaje en el estado'
-}
+//Teniendo en cuenta que estos cntadores siempre estan en cero
+// const INITIAL_COUNTER_STATE = { PODREMOS QUITAR ESTO Y TRABAJAR APARTIR DE setClicks
+//   left: 0,
+//   right:0,
+//   mensaje: 'Mensaje en el estado'
+// }
 
 const App = () => {
-  const [counters, setCounters] = useState(INITIAL_COUNTER_STATE);
+  //PODREMOS QUITAR ESTO Y TRABAJAR APARTIR DE setClicks
+  // const [counters, setCounters] = useState(INITIAL_COUNTER_STATE);
 
-  const [clicks, setClicks] = useState ([])
+  const [clicks, setClicks] = useState ([]) //ahora tenemos más información en este estado
 
   const handleClickLeft = () => {
-    const newCountersState = {
-      ...counters,
-      left: counters.left + 1
-    };
-    //Sin el Spread Operator
-    setCounters(newCountersState);
-    setClicks(prevClicks => {
-      return prevClicks.concat('L')
-    });
+    // const newCountersState = {  PODREMOS QUITAR ESTO Y TRABAJAR APARTIR DE setClicks
+    //   ...counters,
+    //   left: counters.left + 1
+    // };
+    // //Sin el Spread Operator
+    // setCounters(newCountersState);
+    setClicks(prevClicks => ([...prevClicks, ['L']]))
   };
 
   const handleClickRight = () => {
-    setCounters({
-      ...counters,
-      right: counters.right + 1
-    });
-    //Con el Spread Operator
+    // setCounters({  PODREMOS QUITAR ESTO Y TRABAJAR APARTIR DE setClicks
+    //   ...counters,
+    //   right: counters.right + 1
+    // });
     setClicks(prevClicks => ([...prevClicks, ['R']]))
   }
 
-  // una de las formas es pasando los valores originales para volver a ese estado
   const handleReset = () => {
-    setCounters(INITIAL_COUNTER_STATE);
+    // setCounters(INITIAL_COUNTER_STATE); PODREMOS QUITAR ESTO
     setClicks([])
   }
 
+  // Mientras se vayan a filtrar algo así como mil elementos sera mejor esta forma
+  //si llegaran a ser más de 10 mil elementos ya habria que pensarlo
+  //com filter devuelve un string
+  const left = clicks.filter(click => click === 'L')
+  const right = clicks.filter(click => click === 'R')
+  console.log({left});
+
    return (
       <div>
-        {counters.left}
+        {left.length}{/** aquí usamos length para obtener un entero */}
         <button onClick={handleClickLeft}>left</button>
         <button onClick={handleClickRight}>right</button>
-        {counters.right}
+        {right.length}
         <p>
           <button onClick={handleReset}>Reset</button>
         </p>
         <p>Total clicks: {clicks.length}</p>
-        <p>{counters.mensaje}</p>
+        {/* <p>{counters.mensaje}</p> */}
         {clicks.length === 0 ? <WarningNotUsed /> : <ListOfClicks clicks={clicks}/>}
       </div>
     )
