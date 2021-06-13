c860d35e3fb2009279014980a2562e40043c1e2b
Josathan <jonathan.cardozo.dev@gmail.com>
2021-06-12 22:55:07 -0500

    Renderizar listas de elementos en React

--- parte1-react/src/App.js
+++ parte1-react/src/App.js
@@ -1,12 +1,39 @@
 import './styles.css';
 
-const defaulf = function App() {
-return (
-    <div className="App">
-    <h1>Hello CodeSandbox</h1>
-    <h2>Start editing to see some magic happend!</h2>
-  </div>
-  );
-}
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      {
+        notes.map((notes) => {
+          // return <p><strong>{note.id}</strong>{note.content}</p>;
+          return <div><p>
+            <strong>{notes.content}</strong>
+          </p>
+            <p><time>{notes.date}</time></p>
+          </div>
+        })
+      }
+    </div>
+  )
+}
--- parte1-react/src/index.js
+++ parte1-react/src/index.js
@@ -1,76 +1,8 @@
 import ReactDOM from 'react-dom'
-import { useState } from 'react'
 import "./styles.css"
-
-const rootElement = document.getElementById("root");
-
-const WarningNotUsed = () => {
-  return <h1>Todavia no se ha usado el contador</h1>
-}
-const ListOfClicks = ({clicks}) => {
-  return <p>{clicks.join(', ')}</p>
-}
-
-//Teniendo en cuenta que estos cntadores siempre estan en cero
-// const INITIAL_COUNTER_STATE = { PODREMOS QUITAR ESTO Y TRABAJAR APARTIR DE setClicks
-//   left: 0,
-//   right:0,
-//   mensaje: 'Mensaje en el estado'
-// }
-
-const App = () => {
-  //PODREMOS QUITAR ESTO Y TRABAJAR APARTIR DE setClicks
-  // const [counters, setCounters] = useState(INITIAL_COUNTER_STATE);
-
-  const [clicks, setClicks] = useState ([]) //ahora tenemos más información en este estado
-
-  const handleClickLeft = () => {
-    // const newCountersState = {  PODREMOS QUITAR ESTO Y TRABAJAR APARTIR DE setClicks
-    //   ...counters,
-    //   left: counters.left + 1
-    // };
-    // //Sin el Spread Operator
-    // setCounters(newCountersState);
-    setClicks(prevClicks => ([...prevClicks, ['L']]))
-  };
-
-  const handleClickRight = () => {
-    // setCounters({  PODREMOS QUITAR ESTO Y TRABAJAR APARTIR DE setClicks
-    //   ...counters,
-    //   right: counters.right + 1
-    // });
-    setClicks(prevClicks => ([...prevClicks, ['R']]))
-  }
-
-  const handleReset = () => {
-    // setCounters(INITIAL_COUNTER_STATE); PODREMOS QUITAR ESTO
-    setClicks([])
-  }
-
-  // Mientras se vayan a filtrar algo así como mil elementos sera mejor esta forma
-  //si llegaran a ser más de 10 mil elementos ya habria que pensarlo
-  //com filter devuelve un string
-  const left = clicks.filter(click => click === 'L')
-  const right = clicks.filter(click => click === 'R')
-  console.log({left});
-
-   return (
-      <div>
-        {left.length}{/** aquí usamos length para obtener un entero */}
-        <button onClick={handleClickLeft}>left</button>
-        <button onClick={handleClickRight}>right</button>
-        {right.length}
-        <p>
-          <button onClick={handleReset}>Reset</button>
-        </p>
-        <p>Total clicks: {clicks.length}</p>
-        {/* <p>{counters.mensaje}</p> */}
-        {clicks.length === 0 ? <WarningNotUsed /> : <ListOfClicks clicks={clicks}/>}
-      </div>
-    )
-}
+import App from "./App.js"
 
 ReactDOM.render(
-  <App  />,
-  rootElement
-)
+  <App />,
+document.getElementById('root')
+);
