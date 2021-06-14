9e8a29a2c21c6273f39a9e8a86513b6f9a6f5b8a
Josathan <jonathan.cardozo.dev@gmail.com>
2021-06-14 10:58:20 -0500

    Problemas al recorrer listas, pueden venir con tipos de datos incorrectos

--- parte1-react/src/App.js
+++ parte1-react/src/App.js
@@ -1,27 +1,43 @@
 import './styles.css';
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true
-  }
-]
+// Si el valor fuera undefined
+const notes = undefined
+// [
+//   {
+//     id: 1,
+//     content: 'HTML is easy',
+//     date: '2019-05-30T17:30:31.098Z',
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: 'Browser can execute only JavaScript',
+//     date: '2019-05-30T18:39:34.091Z',
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     date: '2019-05-30T19:20:14.298Z',
+//     important: true
+//   }
+// ]
 
 export default function App() {
+  // Haremos esto para evaluar el contenido del array y condicionar lo que retornara
+  if (!notes) {
+    return 'No tenemos notas 1ra forma que evalua si es una valor Falsy'
+  }
+  if (notes.lenght === 0) {
+    return 'No tenemos notas 2da forma revisando las condiciones booleanas'
+  }
+  if (typeof notes === 'undefined') {
+    return 'No tenemos notas 3ra forma alternativa mejor que falsy ya que es más especifica'
+  }
+  if (typeof notes === 'undefined' || notes.lenght === 0) {
+    return 'No tenemos notas 4ta alternativa evaluando dos condiciones'
+  }
+
   return (
     <div>
       {
